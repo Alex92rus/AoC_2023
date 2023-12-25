3bd2bcb86e482fb046496ba32041f9603063f681
Alex92rus <aleksandar.rusinov@gmail.com>
2023-12-25 10:55:50 +0000

    Refactored the code so that there is a module for each question. Added question two

--- aoc_2023/lib/aoc2023.ex
+++ aoc_2023/lib/aoc2023.ex
@@ -1,47 +1,38 @@
 defmodule Aoc2023 do
   @moduledoc """
   Documentation for `Aoc2023`.
+    Main module dealing with the file parsing
   """
+
+  def execute() do
+    readFileLineByLine("./inputs/q1_p1.txt", &Trebutchet.trebutchetPart2/1)
+    readFileLineByLine("./inputs/q1_p1.txt", &Trebutchet.trebutchet/1)
+    readFileLineByLine("./inputs/q2.txt", &CubeConundrum.checkGame/1)
+    readFileLineByLine("./inputs/q2.txt", &CubeConundrum.checkGamePart2/1)
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Aoc2023.hello()
-      :world
+     reading the file and parsing it line by line
   """
-  def readFileLineByLine do
-    file = File.open!("./inputs/q1_sample.txt", [:read, :utf8])
+  def readFileLineByLine(fileName, problemSolved) do
+    file = File.open!(fileName, [:read, :utf8])
 
-    IO.puts solve(:ok, file)
+    IO.puts solve(:ok, file, problemSolved)
 
     File.close(file)
   end
 
-
-  defp solve(:eof, _) do
+  defp solve(:eof, _, _) do
     0
   end
 
-  defp solve(current, file) do
+  defp solve(current, file, problemSolved) do
     aLine = IO.read(file, :line)
 
-    trebutchet(aLine) + solve(aLine, file)
-
+    problemSolved.(aLine) + solve(aLine, file, problemSolved)
   end
 
-  defp trebutchet(:eof) do
-    0
-  end
 
-  defp trebutchet(aLine) do
-    digits = aLine |> String.replace(~r/[^\d]/, "")
-    <<first::binary-size(1), _::binary>> = digits
-    <<_::binary-size(byte_size(digits) - 1), last::binary-size(1)>> = digits
-    Integer.parse(first) |>
-    (fn {num, _} -> num * 10 end).() |>
-    (fn n -> {num, _} = Integer.parse(last); n + num end).()
-  end
+
 
 end
